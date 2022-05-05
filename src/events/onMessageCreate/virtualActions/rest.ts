427743b3ced74966b5d0e46b04f8b66bbb727b03
luxudor <adetera1@gmail.com>
2022-05-05 13:42:42 +0200

    rest function

---
+++ src/events/onMessageCreate/virtualActions/rest.ts
@@ -0,0 +1,13 @@
+import { Client, Message } from "discord.js";
+import VirtualUser from "virtual/models/virtualUser";
+
+export default async (client: Client, message: Message, user: VirtualUser) => {
+    
+
+    let healResult = Math.ceil(Math.random() * 15) + 5;
+    user.hp += healResult;
+    await user.update();
+    let messageBuilder = `**<@${user.id}>** heal for **<@${healResult}>** hp! You have now **<@${user.hp}>** hp.`;
+
+    message.reply(messageBuilder);
+};
