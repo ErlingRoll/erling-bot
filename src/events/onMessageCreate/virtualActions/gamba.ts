136fa7d89be01e66e1a63ec41d23cc8d2573497a
luxudor <adetera1@gmail.com>
2022-05-05 17:12:28 +0200

    added chance to get 100, incresed win/loss and reduced chance

--- src/events/onMessageCreate/virtualActions/gamba.ts
+++ src/events/onMessageCreate/virtualActions/gamba.ts
@@ -1,25 +1,33 @@
 import { Client, Message } from "discord.js";
 import VirtualUser from "virtual/models/virtualUser";
 
-const MAX_WINNINGS = 10;
+const MAX_WINNINGS = 15;
 
 export default async (client: Client, message: Message, user: VirtualUser) => {
     if (user.money < MAX_WINNINGS) {
         return message.reply(`**<@${user.id}>** does not have enough money to gamble.`);
     }
 
-    let gambaResult = Math.floor(Math.random() * 101) + 1;
+    let gambaResult = Math.ceil(Math.random() * 100);
     let gambaAmount = Math.floor(Math.random() * (MAX_WINNINGS + 1));
 
-    const isWinner = gambaResult > 48;
+    const isWinner = gambaResult > 60;
+    const isMax = gambaResult==100;
+
+    let messageBuilder=``;
+    if (isMax){
+        messageBuilder += `\t**Jackpot!**\n`;
+        gambaAmount+=100;
+    }
 
     if (gambaAmount === 0) {
         return message.reply(`**<@${user.id}>** breaks even. Boring.`);
     }
 
     if (isWinner) {
+        messageBuilder+=`**<@${user.id}>** wins ${gambaAmount} money!`;
         user.money += gambaAmount;
-        return message.reply(`**<@${user.id}>** wins ${gambaAmount} money!`);
+        return message.reply(messageBuilder);
     }
 
     if (!isWinner) {
